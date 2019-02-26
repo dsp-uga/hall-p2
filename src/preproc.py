91b2173a02912743a15482c31dc4e2aaf90f4cb6
Priyank Malviya <42504485+priyankmalviya@users.noreply.github.com>
2019-02-26 16:38:39 -0500

    Added comments and changed variable name
    Added few comments to make the code readable and changed the variable mean_img to mean_value, for better readability.

--- src/preproc.py
+++ src/preproc.py
@@ -1,17 +1,21 @@
 import numpy as np
     
 def mean(image):
+'''
+This function takes an image and return the mean value of its pixels.
+'''
     return image.mean()
         
 
 
 def mean_center(image):
-
+''' 
+This function takes as an input an image and center it around 
+the mean value of the pixels and returns the image.
+'''
     #converting image to np.array
     img = np.array(image)
+    mean_value = mean(img)
     
-    mean_img = mean(img)
-
-
-    return mean_img - img 
+    return mean_value - img 
 
