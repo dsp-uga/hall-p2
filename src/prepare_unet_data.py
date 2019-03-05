95f8b04f8976cc9cd11fd0196b04f172c805238f
jayant1234 <parasharjjjayant@gmail.com>
2019-03-05 17:17:35 -0500

    created methods for unets and shifted legacy code

---
+++ src/prepare_unet_data.py
@@ -0,0 +1,19 @@
+
+import preprocess_unet as prunet
+
+
+def train_unet_model(data,mask_list):
+    train_data = prunet.shape_img_data(data)
+    masks = prunet.shape_mask_data(mask_list)
+    model = unet()
+    #Fitting and saving model
+    model.fit(train_data, masks, batch_size=2, epochs=15, verbose=1, shuffle=True)
+    model.save("../dataset/model.h5")
+
+def test_unet_model(data):
+    test_data = shape_img_data(data)
+    model = unet()
+    model.load_weights('../dataset/model.h5')
+
+    prediction = model.predict(test_data, batch_size =4, verbose = 1)
+    np.save('../dataset/prediction.npy', prediction)
