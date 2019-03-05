1c7156aeab03818712928be009d84c9937ff7a96
Jayant Parashar <31920414+Jayant1234@users.noreply.github.com>
2019-03-05 18:29:57 -0500

    Add files via upload

---
+++ src/downloader.py
@@ -0,0 +1,46 @@
+import os
+
+
+#if there is no dataset folder, creating one.  
+if not os.path.exists('../dataset'):
+    os.makedirs('../dataset')
+	
+def text_download():
+	"""
+	downloads train.txt and test.txt files from google cloud links : gs://uga-dsp/project2/test.txt
+	"""
+	
+	command = 'gsutil cp gs://uga-dsp/project2/test.txt'+' ../dataset/'
+	#commands are executed
+	os.system(command)
+	
+	command2 = 'gsutil cp gs://uga-dsp/project2/train.txt'+' ../dataset/'
+	os.system(command2)
+
+ 
+
+def data_download():
+	"""
+	Downloads data folder from google cloud , which contains folders for 300+ video frames of Celia
+	"""
+	#if data folder does not exist, it is created
+	if not os.path.exists('../dataset/data'):
+		os.makedirs('../dataset/data')
+
+	command = 'gsutil cp gs://uga-dsp/project2/data/*'+' ../dataset/data/'
+	os.system(command)
+
+
+
+def masks_download():
+	"""
+	Downloads masks for 210+ train images , which are used as labels for training 
+	"""
+	#if masks folder does not exist, it is created. 
+	if not os.path.exists('../dataset/masks'):
+		os.makedirs('../dataset/masks')
+
+	command = 'gsutil cp gs://uga-dsp/project2/masks/*'+' ../dataset/masks/'
+	os.system(command)
+
+
