d31aca08c1143f0d2c84734ff8fc7f18dffd3d53
Priyank Malviya <42504485+priyankmalviya@users.noreply.github.com>
2019-03-05 23:32:30 -0500

    Comments and slight modification
    Added comments and used object pre to call the functions

--- src/preprocess_unet.py
+++ src/preprocess_unet.py
@@ -5,14 +5,17 @@ import numpy as np
 
 
 def normalize_img_unet(data):
+    '''
+    This function takes as input the data as list of images and normalizes it.
+    '''
     normalize_unet_data = []
     for j in range(0,len(data)):
-        min_val = np.amin(data[j])
-        max_val = np.amax(data[j])
-        range_val = max_val - min_val
-        norm_img = data[j] - min_val/range_val
-        normalize_unet_data.append(norm_img)
-        del norm_img
+        min_val = np.amin(data[j]) # finding the maximum value of the image
+        max_val = np.amax(data[j]) # finding the minimum value of the image
+        range_val = max_val - min_val # finding the range of pixel values in the image
+        norm_img = data[j] - min_val/range_val # normalizing the image
+        normalize_unet_data.append(norm_img) # appending the normalized image to the list
+        del norm_img # deleting the current image from the memory
     return normalize_unet_data
 
 def create_unet_data(img_list):
@@ -24,26 +27,34 @@ def create_unet_data(img_list):
     return train_nparray
 
 def shape_img_data(data):
-    merged_data = pre.merge_images(data)
-    del whole_data
+    '''
+    This function takes as data as list of list and fit the data to be passed to the model.
+    Returns the modified data
+    '''
+    merged_data = pre.merge_images(data) # calling the function to merge 100 images into 1
+    del whole_data # deleting the original data from the memory
 
-    normalize_data = normalize_img_unet(merged_data)
-    del merged_data
+    normalize_data = normalize_img_unet(merged_data) #  calling the function to normalize the merged images
+    del merged_data #deleting the merged images from the memory
 
-    reshaped_data = pre.reshape_image(normalize_data,256,256)
-    del normalize_data
+    reshaped_data = pre.reshape_image(normalize_data,256,256) # calling the function to reshape the normalized data
+    del normalize_data # deleting the normalized data from the memory
 
-    axis_img = pre.add_axis(reshaped_data)
-    train_unet = create_unet_data(axis_img)
-    del axis_img
-    return train_unet
+    axis_img = pre.add_axis(reshaped_data) # calling the function to add axis to the images
+    train_unet = create_unet_data(axis_img) 
+    del axis_img # deleting the previous data from the memory
+    return train_unet 
 
 def shape_mask_data(mask_list):
-    reshaped_mask = reshape_image(mask_list)
-    del mask_list
+    '''
+    This function takes as input the list of images i.e masks and process these images to pass it to the model.
+    Returns the modified images as a list.
+    '''
+    reshaped_mask = pre.reshape_image(mask_list,256,256) # calling the function to reshape the image
+    del mask_list  # deleting the original images from the memory
 
-    mask_axis = add_axis(reshaped_mask)
-    del reshaped_mask
+    mask_axis = pre.add_axis(reshaped_mask) # calling the function to add axis to the images
+    del reshaped_mask # deleting the previous images from the memory
 
     train_mask_unet = create_unet_data(mask_axis)
     return train_mask_unet
